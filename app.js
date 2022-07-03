d4fd0483d636d29ff62d21116546b59df02af285
javokhirm <javokhirmdev@gmail.com>
2022-07-03 20:59:39 +0500

    cors fixed

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const config = require("./src/config");
 const { handleError, invalidRoute } = require("./src/helpers/error");
 const router = require("./src/routes");
 require("./src/helpers/db");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
 
-app.use(cors());
 app.use(handleError);
 app.use(invalidRoute);
 app.listen(config.PORT, () => {
